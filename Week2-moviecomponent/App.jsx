03aefdd29142bdec6c585a2289198d2e2014028a
daeunpk <165495816+daeunpk@users.noreply.github.com>
2024-10-30 03:39:58 +0900

    [박다은/다나] 2주차 정리내용 제출

---
+++ Week2-moviecomponent/App.jsx
@@ -0,0 +1,23 @@
+import Movie from "./components/Movie";
+import { dummy } from "./mocks/movies";
+
+function App() {
+    return (
+        <div>
+        <div className="app-container">
+            {dummy.results.map((item) => {
+            return (
+                <Movie
+                key={item.id}
+                // title={item.title}
+                poster_path={item.poster_path}
+                // vote_average={item.vote_average}
+                />
+            );
+            })}
+        </div>
+        </div>
+    );
+}
+
+export default App;

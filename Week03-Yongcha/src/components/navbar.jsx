3c0c69c9f3fd817b9c019672173c9daeec2d16b3
Jinx921 <hyejin020921@sookmyung.ac.kr>
2024-11-06 02:43:22 +0900

    Update navbar.jsx

--- Week03-Yongcha/src/components/navbar.jsx
+++ Week03-Yongcha/src/components/navbar.jsx
@@ -2,45 +2,56 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
-const NavbarContainer = styled.div`
-  background-color: #1e1e1e;
+const NavbarContainer = styled.nav`
   display: flex;
   justify-content: space-between;
-  padding: 10px 20px;
   align-items: center;
+  padding: 15px 20px;
+  background-color: #111;
   color: white;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  z-index: 10;
 `;
 
 const Logo = styled(Link)`
   font-size: 24px;
-  font-weight: bold;
-  color: #ff007f;
+  color: #ff357e;
   text-decoration: none;
+  font-weight: bold;
 `;
 
-const Button = styled(Link)`
+const NavButtons = styled.div`
+  display: flex;
+  gap: 10px;
+  margin-right: 40px;
+`;
+
+const NavButton = styled(Link)`
+  font-size: 16px;
   color: white;
-  background-color: #ff007f;
-  padding: 5px 10px;
-  border-radius: 5px;
   text-decoration: none;
-  margin-left: 10px;
+  background-color: #ff357e;
+  padding: 8px 16px;
+  border-radius: 5px;
 
   &:hover {
-    background-color: #e60073;
+    background-color: #ff1a60;
   }
 `;
 
-function Navbar() {
+const Navbar = () => {
   return (
     <NavbarContainer>
       <Logo to="/">YONGCHA</Logo>
-      <div>
-        <Button to="/login">로그인</Button>
-        <Button to="/signup">회원가입</Button>
-      </div>
+      <NavButtons>
+        <NavButton to="/login">로그인</NavButton>
+        <NavButton to="/signup">회원가입</NavButton>
+      </NavButtons>
     </NavbarContainer>
   );
-}
+};
 
 export default Navbar;

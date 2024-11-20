7d3915414a762856e7a2aa75366da9d48d9eec8b
Jinx921 <hyejin020921@sookmyung.ac.kr>
2024-11-21 04:09:38 +0900

    Update Authcontext.jsx

--- Week08-todolist/src/context/Authcontext.jsx
+++ Week08-todolist/src/context/Authcontext.jsx
@@ -5,8 +5,11 @@ export const AuthContext = createContext();
 export const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
 
+  const login = (userData) => setUser(userData);
+  const logout = () => setUser(null);
+
   return (
-    <AuthContext.Provider value={{ user }}>
+    <AuthContext.Provider value={{ user, login, logout }}>
       {children}
     </AuthContext.Provider>
   );

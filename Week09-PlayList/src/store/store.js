e126f4815b867dfbcfc887596c65c8ae5cbf9d78
Jinx921 <hyejin020921@sookmyung.ac.kr>
2024-11-26 20:32:32 +0900

    [Week09-2] Update store.js

--- Week09-PlayList/src/store/store.js
+++ Week09-PlayList/src/store/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import cartReducer from '../slice/cartSlice';
+import modalReducer from '../slice/modalSlice';
 
 const store = configureStore({
   reducer: {
     cart: cartReducer,
+    modal: modalReducer,
   },
 });
 

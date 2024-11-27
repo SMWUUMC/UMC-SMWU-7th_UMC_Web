21c8578b3b764279f6b64e36e730534c7b7b1639
daeunpk <165495816+daeunpk@users.noreply.github.com>
2024-11-27 21:41:51 +0900

    Delete week6/src/components directory

--- week6/src/components/Skeleton/card-list-skeleton.jsx
+++
@@ -1,9 +0,0 @@
-import CardSkeleton from "./card-skeleton";
-
-const CardListSkeleton = () => {
-    return (
-        new Array(20).fill(0).map((_,idx) => <CardSkeleton/>)
-    );
-};
-
-export default CardListSkeleton;
